07bebe4c1f2aec36eb791663263141c5852a5d36
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-07-16 04:33:47 +0600

    button function

--- cart.js
+++ cart.js
@@ -1,17 +1,61 @@
+
+function updateProductNumber(id, sign, totalId) {
+    const productInput = document.getElementById(id);
+    let productNumber = productInput.value;
+
+    if (sign == true) {
+        productNumber = parseInt(productNumber) + 1;
+
+    }
+    else if (sign == false && productNumber > 0) {
+
+        productNumber = parseInt(productNumber) - 1;
+
+
+    }
+    productInput.value = productNumber;
+    // update total 
+    const total = document.getElementById(totalId);
+
+    if (totalId == "iphoneValue") {
+        const totalValue = productNumber * 1219;
+        total.innerText = totalValue;
+    }
+    else if (totalId == "caseValue") {
+        const totalValue = productNumber * 59;
+        total.innerText = totalValue;
+
+    }
+
+
+}
+
+// iPhone 11 Silicone Case - Black quantity 
+
 document.getElementById("case-plus").addEventListener('click', function () {
-    const caseInput = document.getElementById('caseNum');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
+    const caseInput = updateProductNumber('caseNum', true, "caseValue");
+    console.log(caseInput);
 
 
 
 })
 
 document.getElementById("case-minuse").addEventListener('click', function () {
-    const caseInput = document.getElementById('caseNum');
-    const caseNumberText = caseInput.value;
-    const caseNumber = parseInt(caseNumberText)
-    if (caseNumber > 1) {
-        caseInput.value = caseNumber - 1;
-    }
+    const caseInput = updateProductNumber('caseNum', false, "caseValue");
+
+})
+
+
+// iPhone 11 128GB Black quentity
+document.getElementById("iphone-minus").addEventListener('click', function () {
+    const iphoneNum = updateProductNumber("iphoneNum", false, "iphoneValue")
+})
+document.getElementById("iphone-plus").addEventListener('click', function () {
+    const iphoneNum = updateProductNumber("iphoneNum", true, "iphoneValue");
+})
+
+
+// clear button 
+document.getElementById('remove').addEventListener('click', function () {
+    console.log("click remove button");
 })
